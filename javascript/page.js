f2eff856af858d42f70e4beaee08f59270387729
Dave Miller <dwmiller@umich.edu>
2011-11-04 11:49:12 -0700

    finish building page dynamically and update variable names.

--- javascript/page.js
+++ javascript/page.js
@@ -1,81 +1,84 @@
 $(function(){
-  edit();
-  $('#setup legend').click(function() {$(this).siblings().toggle();});
+  build_form();
 });
 
-function dict_field(variable,label,is_numeric) {
-  is_numeric = typeof(is_numeric) == 'undefined' ? false : true;
-  document.write("<dt><label for='"+variable+"'>"+label+"</label></dt>");
-  document.write("<dd><input type='text' name='"+variable+"' value='' " + (is_numeric ? "pattern='[0-9]*'" : '') + "/></dd>");
+function attributes_to_s(attributes, defaults) {
+  if (typeof(attributes) == 'undefined') attributes = {};
+  for (var i in defaults) {
+    attributes[i] = attributes[i] || defaults[i];
+  }
+  attrs = [];
+  for (i in attributes) {
+    attrs.push(i + '="' + attributes[i] + '"');
+  }
+  return attrs.join(' ');
 }
-function date_field(variable,label) {
-  document.write("<dt><label for='"+variable+"'>"+label+"</label></dt>");
-  document.write("<dd><input type='text' name='"+variable+"' id='"+variable+"' value='' class='date'/></dd>");
+function text_field(nickname,label,attributes) {
+  var attrs = attributes_to_s(attributes, {});
+  var html = "<dt><label for='"+nickname+"'>"+label+"</label></dt>";
+  html += "<dd><input type='text' id='"+nickname+"' name='"+nickname+"' "+attrs+"/></dd>";
+  return html;
 }
-function checkbox(variable,label,prefix) {
-  document.write('<div class="checkbox">');
-  document.write("<label for='"+variable+"' onclick='$(\"#"+variable+"\").check()'>"+label+"</label>");
-  document.write("<input type='checkbox' name='"+variable+"' id='"+variable+"' value='"+prefix+"_"+variable+"' checked='checked'/>");
-  document.write("</div>");
+function date_field(nickname,label,attributes) {
+  var attrs = attributes_to_s(attributes, {});
+  var html = "<dt><label for='"+nickname+"'>"+label+"</label></dt>";
+  html += "<dd><input type='text' name='"+nickname+"' id='"+nickname+"' class='date' "+attrs+"/></dd>";
+  return html;
+}
+function checkbox_group_item(nickname,label,prefix,attributes) {
+  var defaults = {checked:'checked'};
+  var attrs = attributes_to_s(attributes, defaults);
+  var html = '<div class="checkbox">';
+  html += "<label for='"+nickname+"' onclick='$(\"#"+nickname+"\").check()'>"+label+"</label>";
+  html += "<input type='checkbox' name='"+nickname+"' id='"+nickname+"' value='"+prefix+"_"+nickname+"' "+attrs+"/>";
+  html += "</div>";
+  return html;
+}
+function checkbox_group(prefix, label, attributes, group){
+  var html = '<dt>'+label+'</dt><dd>';
+  for (var item in group) {
+    var options = group[item];
+    var nickname = options.rename || underscore(item);
+    html += checkbox_group_item(nickname, item, prefix, options.attributes);
+  }
+  html += '</dd>';
+  return html;
+}
+function textarea(nickname, label, attributes, text) {
+  var defaults = {rows:'10',cols:'60'};
+  var attrs = attributes_to_s( attributes, defaults );
+  var html = '<dt><label for="'+nickname+'">'+label+'</label></dt>';
+  html += '<dd><textarea name="'+nickname+'"'+attrs+'>'+text+'</textarea></dd>';
+  return html;
 }
 
-function edit() {
+function build_form() {
   var html = '';
   for (var section in fields) {
     var inputs = fields[section];
     html += '<fieldset class="edgeToEdge">';
     html += '<legend>'+section+'</legend>';
-    html += '<dl class="edgeToEdge formFields" style="display:block;">';
-    for (var input in inputs) {
-      var options = inputs[input];
-      switch(options['type']) {
-        case 'text':
-          html += dict_field(underscore(input), input);
-          break;
+    html += '<dl class="edgeToEdge formFields" style="display:none;">';
+    for (var label in inputs) {
+      var options = inputs[label];
+      var nickname = options.rename || underscore(label);
+      switch(options.type) {
         case 'date':
-          html += date_field(underscore(input), input);
+          html += date_field(nickname, label, options.attributes);
           break;
-        default:
-          // alert('Input type' + input.type +' not found in page#edit');
+        case 'checkbox_group':
+          html += checkbox_group(nickname, label, options.attributes, options.group);
+          break;
+        case 'textarea':
+          html += textarea(nickname, label, options.attributes, options.text);
+          break;
+        default: // text input
+          html += text_field(nickname, label, options.attributes);
       }
     }
+    html += '</dl></fieldset>';
   }
-  html += '</dl></fieldset>';
-  console.info(html);
   $('#setup p').before(html);
-}
-
-function preview() {
-  var substitutions = {};
-  substitutions = hash_add(substitutions, $('#setup input[type=text]'));
-  substitutions = hash_add(substitutions, $('#setup textarea'));
-  substitutions = hash_add(substitutions, $('#setup input[type=hidden]'));
-  substitutions = hash_add_checked(substitutions, $('#setup input:checked'));
-
-  var new_content = jQuery.extend({}, content);
-  for (var j in substitutions) {
-    var reg = new RegExp('«'+j+'»', 'gim');
-    if (substitutions[j] != '') {
-      for (var i in new_content) {
-        new_content[i] = new_content[i].replace(reg,substitutions[j]);
-      }
-    }
-  } 
-  
-  if ($('#preview dt').length > 0) {
-    for (var i in new_content) {
-      $('#' + underscore(i)).html( new_content[i] );
-    }
-  } else {
-    var event_id = substitutions['bride_last'] + '-' + substitutions['groom_last'] + '-' + underscore(substitutions['wedding_date']);
-    var insert = '<input class="hidden" name="event_id" value="'+event_id+'"/>';
-    insert += '<dl class="edgeToEdge formFields">';
-    for (i in new_content) {
-      insert += '<dt><label for="'+underscore(i)+'">' + i + '</label></dt>';
-      insert += '<dd><textarea cols="60" rows="10" name="name" id="'+underscore(i)+'">' + new_content[i] + '</textarea></dd>';
-    }
-    $('#preview p').before(insert + '</dl>');
-  }
 }
 
 function hash_add(hash, elements) {
@@ -103,4 +106,35 @@ function hash_add_checked(hash, elements) {
     hash[i] = lists[i].join(', ');
   }
   return hash;
+}
+
+function preview() {
+  var substitutions = {};
+  substitutions = hash_add(substitutions, $('#setup input[type=text]'));
+  substitutions = hash_add(substitutions, $('#setup textarea'));
+  substitutions = hash_add(substitutions, $('#setup input[type=hidden]'));
+  substitutions = hash_add_checked(substitutions, $('#setup input:checked'));
+
+  var new_content = jQuery.extend({}, content);
+  for (var j in substitutions) {
+    var reg = new RegExp('«'+j+'»', 'gim');
+    if (substitutions[j] !== '') {
+      for (var i in new_content) {
+        new_content[i] = new_content[i].replace(reg,substitutions[j]);
+      }
+    }
+  }
+
+  if ($('#preview dt').length > 0) {
+    for (var k in new_content) {
+      $('#' + underscore(k)).html( new_content[k] );
+    }
+  } else {
+    insert = '<dl class="edgeToEdge formFields">';
+    for (var m in new_content) {
+      insert += '<dt><label for="'+underscore(m)+'">' + m + '</label></dt>';
+      insert += '<dd><textarea cols="60" rows="10" name="name" id="'+underscore(m)+'">' + new_content[m] + '</textarea></dd>';
+    }
+    $('#preview p').before(insert + '</dl>');
+  }
 }
