79776b5c20089ff9e621ff72ac035ad59acd50de
Dave Miller <dwmiller@umich.edu>
2011-11-23 13:07:19 -0800

    ajax submit data

--- public/javascript/builder.js
+++ public/javascript/builder.js
@@ -22,10 +22,17 @@ function hide_message() {
   $('#message').html('');
 }
 
+function delete_doc(id, doit) {
+  if (doit) {
+    $('#doc_'+id).remove();
+    $.get('/delete/'+id).error(function() {alert("Error: document not deleted.");});
+  }
+  return false;
+}
+
 function save_all() {
-  $('form').attr('target','_blank');
-  $('form').attr('action','save/' + filename(''));
-  return true;
+  ajax_submit('save');
+  return false;
 }
 
 function email_pdf() {
@@ -36,9 +43,22 @@ function email_pdf() {
     var html = '<input type="hidden" name="email_to" id="email_to" value="'+escape(email_to)+'"/>';
     $('#preview p').eq(0).before(html);
   }
-  $('form').attr('target','_blank');
-  $('form').attr('action','email/' + filename(''));
-  return true;
+  ajax_submit('email');
+  return false;
+}
+
+function ajax_submit(path) {
+  var jqxhr = $.post(path+'/'+filename(''),
+        $("form").serialize(),
+        function() {alert("success");}
+      )
+      .success(function() { alert("second success"); })
+      .error(function() { alert("error"); })
+      .complete(function() { alert("complete"); });
+  // perform other work here ...
+
+  // Set another completion function for the request above
+  jqxhr.complete(function(){ alert("second complete"); });
 }
 
 function edit_details() {
@@ -56,8 +76,7 @@ function preview_as_html() {
 function download_pdf() {
   $('form').removeAttr('target');
   $('form').attr('action', filename('pdf'));
-  console.info($('form'));
-  return false;
+  return true;
 }
 
 function review_content() {
