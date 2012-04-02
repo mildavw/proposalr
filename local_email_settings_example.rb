4b22915ca654b2fc9b1cd8e8a0338104c50c7f64
Dave Miller <dwmiller@umich.edu>
2012-04-02 13:16:04 -0700

    Add a local email settings example file.

---
+++ local_email_settings_example.rb
@@ -0,0 +1,5 @@
+# fill in your info and rename this local_email_settings.rb
+set :email_username, 'user_9000'
+set :email_password, 'asdf90823ljk'
+set :email_service,  'gmail.com'
+set :email_domain,   'localhost.localdomain'
